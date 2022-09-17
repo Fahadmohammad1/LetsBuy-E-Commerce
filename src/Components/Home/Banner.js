a97dabb114976c7f06878478564bd07351eaebff
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-09-17 19:53:32 +0600

    new pic added in banner

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -5,17 +5,18 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./BannerStyle.css";
 import { Autoplay, Pagination } from "swiper";
-import image1 from "../../Assets/images/headphone.png";
-import image2 from "../../Assets/images/xbox.png";
-import image3 from "../../Assets/images/tv.png";
+import image1 from "../../Assets/images/banner/headphone.png";
+import image2 from "../../Assets/images/banner/xbox.png";
+import image3 from "../../Assets/images/banner/tv.png";
 import bag from "../../Assets/images/banner/bag.jpg";
 import watch from "../../Assets/images/banner/watch.jpg";
 import shoes from "../../Assets/images/banner/shoes.jpg";
 import headphone from "../../Assets/images/banner/headphone.jpg";
+import sale from "../../Assets/images/banner/discount-2.jpg";
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-4 gap-3 px-5 h-screen ">
+    <div className="grid grid-cols-4 gap-3 px-5 h-screen">
       <div className="">
         <div className="card w-auto bg-emerald-50 shadow-xl mb-3">
           <img src={bag} alt="bag" />
@@ -40,22 +41,9 @@ const Banner = () => {
             className="mySwiper"
           >
             <SwiperSlide>
-              <div>
-                <div className="text-left">
-                  <h1 className="text-5xl font-bold">Mega LCD TV For Sports</h1>
-                  <p>
-                    This is the best tv in the world for people who just want to
-                    waste time in front of tv.
-                  </p>
-                  <h1 class="">$1200</h1>
-                  <div class="btn bg-[#FF4E16]">Buy Now</div>
-                </div>
-              </div>
-              <div className="w-1/2">
-                <img className="py-10" src={image1} alt="" />
-              </div>
+              <img src={sale} alt="" />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide className="pl-4">
               <div>
                 <div className="text-left">
                   <h1 className="text-5xl font-bold">Mega LCD TV For Sports</h1>
@@ -63,23 +51,7 @@ const Banner = () => {
                     This is the best tv in the world for people who just want to
                     waste time in front of tv.
                   </p>
-                  <h1 class="">$1200</h1>
-                  <div class="btn bg-[#FF4E16]">Buy Now</div>
-                </div>
-              </div>
-              <div className="w-1/2">
-                <img src={image2} alt="" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div>
-                <div className="text-left">
-                  <h1 className="text-5xl font-bold">Mega LCD TV For Sports</h1>
-                  <p>
-                    This is the best tv in the world for people who just want to
-                    waste time in front of tv.
-                  </p>
-                  <h1 class="">$1200</h1>
+                  <h1 class="text-pink-500">$1200</h1>
                   <div class="btn bg-[#FF4E16]">Buy Now</div>
                 </div>
               </div>
@@ -95,7 +67,7 @@ const Banner = () => {
                     This is the best tv in the world for people who just want to
                     waste time in front of tv.
                   </p>
-                  <h1 class="">$1200</h1>
+                  <h1 class="text-pink-500">$1200</h1>
                   <div class="btn bg-[#FF4E16]">Buy Now</div>
                 </div>
               </div>
