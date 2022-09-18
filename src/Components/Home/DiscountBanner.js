e295fdfb3821e0316f58d96bc83af0a28e4e47bf
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-09-18 12:25:47 +0600

    discount banner done

--- src/Components/Home/DiscountBanner.js
+++ src/Components/Home/DiscountBanner.js
@@ -2,13 +2,18 @@ import React from "react";
 
 const DiscountBanner = () => {
   return (
-    <div className="container mx-auto mt-20">
+    <div
+      data-aos="flip-up"
+      data-aos-easing="linear"
+      data-aos-duration="1000"
+      className="container mx-auto mt-16"
+    >
       <div class="p-6 py-12 bg-pink-500 text-gray-900 rounded-full">
         <div class="container mx-auto">
           <div class="flex flex-col lg:flex-row items-center justify-between">
             <h2 class="text-center text-6xl tracking-tighter font-bold">
               Up to <br class="sm:hidden" />
-              <span className="text-white">50</span>% Off
+              <span className="text-white">50</span>% Cashback
             </h2>
             <div class="space-x-2 text-center py-2 lg:py-0">
               <span>Plus free shipping! Use code:</span>
