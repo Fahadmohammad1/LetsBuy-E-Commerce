e1eaaddd52d8d0107b76efe0882e7a64be5399a9
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-09-24 18:46:51 +0600

    categories responsive

--- src/Components/Home/NewProduct.js
+++ src/Components/Home/NewProduct.js
@@ -38,6 +38,42 @@ const NewProduct = () => {
   };
   return (
     <Slider {...settings}>
+      <div className="card w-auto border-2 badge">
+        <figure className="px-8 pt-8 ">
+          <img
+            src="https://www.pngall.com/wp-content/uploads/2016/05/Laptop-PNG-File.png"
+            alt="laptop"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="pt-3 pl-3">
+          <h2 className="card-title">M2 Bionic - Apple</h2>
+          <p>(laptop) - Computer and Accessories</p>
+          <div className="card-actions w-full pr-3 pb-3">
+            <button className="btn w-full rounded-3xl text-[#FF4E16] bg-white">
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-auto border-2">
+        <figure className="px-8 pt-8 ">
+          <img
+            src="https://www.pngall.com/wp-content/uploads/2016/05/Laptop-PNG-File.png"
+            alt="laptop"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="pt-3 pl-3">
+          <h2 className="card-title">M2 Bionic - Apple</h2>
+          <p>(laptop) - Computer and Accessories</p>
+          <div className="card-actions w-full pr-3 pb-3">
+            <button className="btn w-full rounded-3xl text-[#FF4E16] bg-white">
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="card w-auto border-2">
         <figure className="px-8 pt-8 ">
           <img
