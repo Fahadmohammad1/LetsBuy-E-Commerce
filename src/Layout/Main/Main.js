06c110fa1cfce32fd370e103e1ac8083236e1b68
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-01-26 19:43:44 +0600

    routes updated to new version

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../../Shared/Footer";
+import Header from "../../Shared/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Counter } from "../../features/counter/Counter";
+
 import Banner from "./Banner";
 import DiscountBanner from "./DiscountBanner";
 import Gallery from "./Gallery";
 import ItemHome from "./ItemHome";
 import NewProducts from "./NewProducts";
 import Shop from "./Shop";
-// import Shops from "./Shops";
 
 const Home = () => {
   return (
@@ -17,7 +16,6 @@ const Home = () => {
       <ItemHome />
       <Shop />
       <Gallery />
-      <Counter />
     </div>
   );
 };
