b13b908d838d3341561af82b62f1e48ea8b5a658
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-01-29 20:47:05 +0600

    product reducer & thunk added

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { LOAD_PRODUCTS } from "../actionTypes/actionTypes";
+
+const initialState = {
+  products: [],
+};
+
+const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
