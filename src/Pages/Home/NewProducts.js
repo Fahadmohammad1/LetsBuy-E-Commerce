f6a659b596a0a87f1565c527fcfc155783a6d938
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-06 20:53:53 +0600

    card responsive done

--- src/Pages/Home/NewProducts.js
+++ src/Pages/Home/NewProducts.js
@@ -33,9 +33,9 @@ const NewProducts = () => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
         },
       },
       {
