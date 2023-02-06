ecb73c6ac7c130107e372229eddff07aac2effe3
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-06 21:04:37 +0600

    2nd product card added

--- src/Pages/Home/PopularProducts.js
+++ src/Pages/Home/PopularProducts.js
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
-import PopularProduct from "./PopularProduct";
+import React from "react";
+import { useSelector } from "react-redux";
+import ProductCardTwo from "../../Components/ProductCardTwo";
 
 const PopularProducts = () => {
+  const products = useSelector((state) => state?.product?.products);
   return (
     <section className="container mx-auto my-10">
       <h2 className="font-bold text-3xl border-b-2 border-primary inline-block text-gray-800">
         Popular
       </h2>
       <hr />
-      <div className="grid md:grid-cols-4 gap-4 px-5"></div>
+      <div className="grid md:grid-cols-4">
+        {products.map((product) => (
+          <ProductCardTwo key={product.key} product={product} />
+        ))}
+      </div>
     </section>
   );
 };
