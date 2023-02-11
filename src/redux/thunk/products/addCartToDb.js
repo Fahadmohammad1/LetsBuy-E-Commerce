5aac9b3ee15958cda8cbfe46535818b9fff77643
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-11 21:25:16 +0600

    email added to post

--- src/redux/thunk/products/addCartToDb.js
+++ src/redux/thunk/products/addCartToDb.js
@@ -1,18 +1,21 @@
+import { toast } from "react-hot-toast";
 import { addToCart } from "../../actions/productAction";
 
 const addCartToDb = (product, email) => {
   return async (dispatch, getState) => {
+    const cartItem = { ...product, email: email };
     const res = await fetch(`http://localhost:5000/v1/cart-item/${email}`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(product),
+      body: JSON.stringify(cartItem),
     });
     const data = await res.json();
 
     if (data.success) {
       dispatch(addToCart(product));
+      toast.success(data.message);
     }
 
     console.log(data);
