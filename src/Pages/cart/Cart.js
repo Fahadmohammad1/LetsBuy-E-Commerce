33102afc20bd3812b9fbc2cb39675d9e6462304d
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-11 22:03:25 +0600

    cart data load done

--- src/Pages/cart/Cart.js
+++ src/Pages/cart/Cart.js
@@ -1,9 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../Components/ProductCard";
+import auth from "../../firebase/firebase.init";
+import fetchCartData from "../../redux/thunk/products/fetchCartData";
 
 const Cart = () => {
   const products = useSelector((state) => state.product.cart);
+  const dispatch = useDispatch();
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    dispatch(fetchCartData(user.email));
+  }, [user.email, dispatch]);
   return (
     <section className="container mx-auto">
       <h3 className="my-10 text-xl font-bold uppercase text-center">
--- src/redux/thunk/products/fetchCartData.js
+++ src/redux/thunk/products/fetchCartData.js
@@ -1,5 +1,10 @@
-const fetchCartData = () => {
-  return async () => {
-    const data = fetch("");
+const fetchCartData = (email) => {
+  return async (dispatch, getState) => {
+    const res = await fetch(`http://localhost:5000/v1/cart-item/${email}`);
+    const data = await res.json();
+
+    console.log(data);
   };
 };
+
+export default fetchCartData;
