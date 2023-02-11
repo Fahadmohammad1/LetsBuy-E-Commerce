912b4c4c6222c1dccc1d86de41a0d87f6bd83794
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-11 23:08:19 +0600

    cart item changed post to put

--- src/redux/thunk/products/addCartToDb.js
+++ src/redux/thunk/products/addCartToDb.js
@@ -4,13 +4,16 @@ import { addToCart } from "../../actions/productAction";
 const addCartToDb = (product, email) => {
   return async (dispatch, getState) => {
     const cartItem = { ...product, email: email };
-    const res = await fetch(`http://localhost:5000/v1/cart-item/${email}`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(cartItem),
-    });
+    const res = await fetch(
+      `http://localhost:5000/v1/cart-item/${product._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(cartItem),
+      }
+    );
     const data = await res.json();
 
     if (data.success) {
