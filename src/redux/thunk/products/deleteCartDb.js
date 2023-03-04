0efa828ff84d164f7a83d9a8798ca2fbd378a259
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-03-04 23:10:55 +0600

    delete api set with email & id

--- src/redux/thunk/products/deleteCartDb.js
+++ src/redux/thunk/products/deleteCartDb.js
@@ -1,10 +1,11 @@
-const deleteCartDb = () => {
+const deleteCartDb = (product, email) => {
   return async (dispatch, getState) => {
-    const res = await fetch(``, {
+    const res = await fetch(`http://localhost:5000/v1/cart-item/${email}`, {
       method: "DELETE",
       Headers: {
         "Content-type": "application/json",
       },
+      body: JSON.stringify(product.productId),
     });
 
     const data = res.json();
